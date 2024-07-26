1389128de543ac0298d3dd18823179af194128d3
cwp0 <wenpengchen@njust.edu.cn>
2024-07-26 14:56:29 +0800

    实现秒杀下单功能

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
+    /**
+     * @Description: 秒杀代金券
+     * @Param: voucherId      {java.lang.Long}
+     * @Return: com.hmdp.dto.Result
+     * @Author: cwp0
+     * @CreatedTime: 2024/7/26 14:38
+     */
+    Result seckillVoucher(Long voucherId);
 }
